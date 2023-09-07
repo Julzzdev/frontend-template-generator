5d89126d445fc1688dfa7162fffd2a024111cfb9
Julio Olvera <jolvera@foundationmedicine.com>
2023-09-07 12:07:25 -0600

    :tada: first commit

---
+++ templates/vite-react-tailwind-template/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex w-full justify-center">
+        <h1 className="text-3xl">Vite + React + Tailwindcss</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ templates/vite-vue-tailwind-template/src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="flex w-full">
+    <h1 class="text-3xl">Vite + Vue + Tailwind</h1>
+  </div>
+</template>
